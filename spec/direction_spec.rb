5039323a64c9832dd3ac7dc20cd8939ae315ce0d
Kevin Yang <kevieyang@gmail.com>
2014-04-16 00:49:53 +1000

    added more direction tests

--- spec/direction_spec.rb
+++ spec/direction_spec.rb
@@ -9,16 +9,52 @@ describe Direction do
 	dir.facing.should == "NORTH"
   end
   
-  it "turns the robot left" do
+  it "turns the robot left from NORTH" do
     dir = Direction.new("NORTH")
 	dir.left
 	dir.facing.should == "WEST"
   end
   
-  it "turns the robot right" do
+  it "turns the robot right from NORTH" do
     dir = Direction.new("NORTH")
 	dir.right
 	dir.facing.should == "EAST"
   end
 
+  it "turns the robot left from SOUTH" do
+    dir = Direction.new("SOUTH")
+	dir.left
+	dir.facing.should == "EAST"
+  end
+  
+  it "turns the robot right from SOUTH" do
+    dir = Direction.new("SOUTH")
+	dir.right
+	dir.facing.should == "WEST"
+  end
+  
+  it "turns the robot left from EAST" do
+    dir = Direction.new("EAST")
+	dir.left
+	dir.facing.should == "NORTH"
+  end
+  
+  it "turns the robot right from EAST" do
+    dir = Direction.new("EAST")
+	dir.right
+	dir.facing.should == "SOUTH"
+  end
+  
+  it "turns the robot left from WEST" do
+    dir = Direction.new("WEST")
+	dir.left
+	dir.facing.should == "SOUTH"
+  end
+  
+  it "turns the robot right from WEST" do
+    dir = Direction.new("WEST")
+	dir.right
+	dir.facing.should == "NORTH"
+  end
+  
 end
