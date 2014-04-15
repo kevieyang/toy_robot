0e368841b3a959d038ac280c3465b228cdecd147
Kevin Yang <kevieyang@gmail.com>
2014-04-16 01:05:53 +1000

    adding skeleton robot files

---
+++ robot.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+class Robot
+
+  def initialize()
+
+  end
+
+  def place
+  end
+  
+  def move
+  end
+  
+  def left
+  end
+  
+  def right
+  end
+  
+  attr_reader :position, :direction
+
+end
---
+++ spec/robot_spec.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'rspec'
+require '../robot.rb'
+
+describe Robot do
+
+  
+end
